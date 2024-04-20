f2702ce144514a1e0b4c6cbaa2a7675203137461
TATHAN <costa.tathan@gmail.com>
2024-04-20 12:21:06 +0200

    Add command

--- src/main/java/fr/tathan/provincesounds/commands/ProvinceSoundCommand.java
+++ src/main/java/fr/tathan/provincesounds/commands/ProvinceSoundCommand.java
@@ -8,6 +8,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
+import java.util.List;
+
 public class ProvinceSoundCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
@@ -17,16 +19,28 @@ public class ProvinceSoundCommand implements CommandExecutor {
 
         if(strings[0].equalsIgnoreCase("random")) {
             if(strings.length == 1) {
-                commandSender.sendMessage("You need to specify a random list!");
+                sendMessage(commandSender, "You need to specify a random list!");
                 return false;
             }
 
             FileConfiguration config = ProvinceSounds.getInstance().getConfig();
-            String random = strings[1];
+            String playlist = strings[1];
 
-            Utils.playSound((Player) commandSender, Utils.getRandomSound(config.getConfigurationSection("randoms").getStringList(random)));
+            List<String> soundList = config.getConfigurationSection("playlists").getStringList(playlist);
+
+            if(soundList == null || soundList.isEmpty()) {
+                sendMessage(commandSender, "The " + playlist + " list does not exist!");
+                sendMessage(commandSender, "Available lists: " + config.getConfigurationSection("playlists").getKeys(false));
+                return true;
+            }
+            Utils.playSound((Player) commandSender, Utils.getRandomSound(soundList));
             return true;
         }
         return false;
+    }
+
+    public void sendMessage(CommandSender commandSender, String message) {
+        commandSender.sendMessage("§a[§3ProvinceSounds§a]§f " + message);
+
     }
 }
